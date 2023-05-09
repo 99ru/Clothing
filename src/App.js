45e1cbdc735059eda176460530c586dde4647bd8
99ru <yan.99ru@gmail.com>
2023-05-09 19:07:11 +0200

    kk

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+// App.js
 import "./styles/style.scss";
 import "./styles/style.css";
 import Header from "./components/header/Header";
-import Category from "./components/category/category";
-import NewArrivals from "./components/products/NewArrivals";
 import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+/* import MensPage from "./pages/MensPage";
+import WomensPage from "./pages/WomensPage";
+import CartPage from "./pages/CartPage"; */
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Category />
-      <NewArrivals />
-      <Footer />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        {/*   <Route path="/mens" element={<MensPage />} />
+          <Route path="/womens" element={<WomensPage />} />
+          <Route path="/cart" element={<CartPage />} /> */}
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
