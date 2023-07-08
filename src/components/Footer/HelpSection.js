a722b52603686e72769923e7d3ed2e513fbcb10d
99ru <yan.99ru@gmail.com>
2023-07-08 13:12:48 +0200

    href fix

--- src/components/Footer/HelpSection.js
+++ src/components/Footer/HelpSection.js
@@ -4,10 +4,10 @@ const HelpSection = () => (
   <div className="help-section">
     <h2>Help</h2>
     <ul>
-      <li><a href="#">Shipping</a></li>
-      <li><a href="#">Returns</a></li>
-      <li><a href="#">Order Status</a></li>
-      <li><a href="#">FAQ</a></li>
+      <li><a href="!#">Shipping</a></li>
+      <li><a href="!#">Returns</a></li>
+      <li><a href="!#">Order Status</a></li>
+      <li><a href="!#">FAQ</a></li>
     </ul>
   </div>
 );
--- src/components/Footer/MoreSection.js
+++ src/components/Footer/MoreSection.js
@@ -4,10 +4,10 @@ const MoreSection = () => (
   <div className="more-section">
     <h2>More</h2>
     <ul>
-      <li><a href="#">About Us</a></li>
-      <li><a href="#">Contact Us</a></li>
-      <li><a href="#">Privacy Policy</a></li>
-      <li><a href="#">Terms & Conditions</a></li>
+      <li><a href="!#">About Us</a></li>
+      <li><a href="!#">Contact Us</a></li>
+      <li><a href="!#">Privacy Policy</a></li>
+      <li><a href="!#">Terms & Conditions</a></li>
     </ul>
   </div>
 );
