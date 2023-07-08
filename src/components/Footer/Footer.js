fef6186fc8b301007f6ff707327d428b565aff3c
99ru <yan.99ru@gmail.com>
2023-07-08 10:34:14 +0200

    css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-content">
-        <h4>© 2023 | Alphalete Athletics | THIS IS NOT A REAL PAGE</h4>
-        <h5>LEARN MORE | DREAM MORE | BE MORE</h5>
+        <p>© 2023 | Alphalete Athletics | Not Real Page</p>
+        <p>LEARN MORE | DREAM MORE | BE MORE</p>
         <a className="href" href="https://alphaleteathletics.com/" target="_blank" rel="noopener noreferrer">Real Page</a>
       </div>
     </div>
